f097645b20c1810f4d44805dcec07705b49c166a
ShriramPrakash <70899686+ShriramPrakash@users.noreply.github.com>
2026-01-30 09:27:51 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,3 @@
 output "ec2_public_ip" {
-  value = aws_instance.devops_vm.public_ip
+  value = aws_instance.gitops_ec2.public_ip
 }
-
-output "ecr_url" {
-  value = aws_ecr_repository.app_repo.repository_url
-}
-
