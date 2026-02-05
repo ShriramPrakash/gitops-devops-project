548621a7d4046d018fcc3c71d9170c9752e42625
ShriramPrakash <70899686+ShriramPrakash@users.noreply.github.com>
2026-02-05 08:00:49 +0530

    Update argocd.tf

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -1,14 +1,19 @@
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
+data "aws_eks_cluster" "this" {
+  name = aws_eks_cluster.this.name
 }
 
 data "aws_eks_cluster_auth" "this" {
   name = aws_eks_cluster.this.name
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+# Helm provider (no kubernetes {} block)
+provider "helm" {}
 
 resource "helm_release" "argocd" {
   name             = "argocd"
@@ -16,5 +21,7 @@ resource "helm_release" "argocd" {
   chart            = "argo-cd"
   namespace        = "argocd"
   create_namespace = true
+
+  depends_on = [aws_eks_node_group.this]
 }
 
