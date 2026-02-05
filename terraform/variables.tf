cd3a373157bdf25b9a8824f350bb5896dda5eae9
ShriramPrakash <70899686+ShriramPrakash@users.noreply.github.com>
2026-02-05 07:57:21 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,3 @@
-variable "ami_id" {
-  description = "AMI ID for EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-}
-
-variable "key_name" {
-  description = "SSH key pair name"
-  type        = string
-}
-
 variable "aws_region" {
   description = "AWS region for EKS cluster"
   type        = string
@@ -27,3 +12,4 @@ variable "private_subnet_ids" {
   description = "Private subnet IDs for EKS"
   type        = list(string)
 }
+
